9dc7ca4f1f6af226ef8e6d10189290e84efe2a0f
ydenzel <30435272+ydenzel@users.noreply.github.com>
2017-07-26 00:20:45 +0800

    Update Registration.php

--- Registration.php
+++ Registration.php
@@ -37,24 +37,38 @@
 	<div class="main-body col-xs-12 col-md-4 col-md-offset-4">
 		<div class="row" id="reg-row">
 			<div class="text-center"><h1><em>Registration Form<em></h1></div>
-			<form>
-				<input class="col-xs-12 form-control" type="text" value="Name">
-				<input class="col-xs-12 form-control" type="text" value="Last Name">
-				<label class="checkbox-inline"><input type="checkbox" value="">Male</label>
-				<label class="checkbox-inline"><input type="checkbox" value="">Female</label>
-				<input class="col-xs-12 form-control" type="text" value="Email Address">
-				<input class="col-xs-12 form-control" type="text" value="Age">
-				<input class="col-xs-12 form-control" type="text" value="Address">
-				<input class="col-xs-12 form-control" type="text" value="Cell number">
-				<hr>
-				<input class="col-xs-12 form-control" type="text" value="Guardian's number">
+			<form action="register.php" method="post">
+				Name<br>
+				<input class="col-xs-12 form-control" type="text" placeholder="Name" name="firstname">
+				<input class="col-xs-12 form-control" type="text" placeholder="Last Name" name="lastname">
+				Choose a username<br>
+				<input class="col-xs-12 form-control" type="text" placeholder="Username" name="username">
+				Enter a password<br>
+				<input class="col-xs-12 form-control" type="password">
+				Confirm password<br>
+				<input class="col-xs-12 form-control" type="password" name="password">
+				<br>Gender<br>
+				<label class="checkbox-inline"><input type="checkbox" placeholder="">Male</label>
+				<label class="checkbox-inline"><input type="checkbox" placeholder="">Female</label>
+				<br><br>Enter Email Address<br>
+				<input class="col-xs-12 form-control" type="text" placeholder="Email Address">
+				Birthday<br>
+				<input class="col-xs-12 form-control" type="text" placeholder="Dropdowns ni diri ples">
+				Enter Home Address<br>
+				<input class="col-xs-12 form-control" type="text" placeholder="Address">
+				Mobile Phone Number
+				<input class="col-xs-12 form-control" type="text" placeholder="Cell number">
+
+				<div class="col-md-1 col-md-offset-1">
+									<button  type="submit" class="btn btn-success">Confirm</button>
+							</div>
 			</form>
 		</div>
 	</div>
 </div>
 
 <!---->
-<div class="sub"><?php include "footer.php";?></div>
+<!-- <div class="sub"><?php include "footer.php";?></div> -->
 
 </body>
 
